7999220b073d4af18f83f00bbb306f1f58c9c8c0
Spica <spicajames@users.noreply.github.com>
2018-08-02 10:34:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,26 +2,5 @@
 header('Content-type: application/json');
 ?>
 {
-  "fulfillmentText": "This is a text response",
-  "fulfillmentMessages": [
-    {
-      "card": {
-        "title": "card title",
-        "subtitle": "card text",
-        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-        "buttons": [
-          {
-            "text": "button text",
-            "postback": "https://assistant.google.com/"
-          }
-        ]
-      }
-    }
-  ],
-  "source": "example.com",
-  "payload": {   
-    "kik": {
-      "text": "Hello, Facebook!"
-    }
-  }
+  "fulfillmentText": "This is a text response" 
 }
