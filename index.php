289997e1a08cd35ceb86214d68252f18871e197f
Spica <spicajames@users.noreply.github.com>
2018-08-02 10:31:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -19,40 +19,9 @@ header('Content-type: application/json');
     }
   ],
   "source": "example.com",
-  "payload": {
-    "google": {
-      "expectUserResponse": true,
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "this is a simple response"
-            }
-          }
-        ]
-      }
-    },
-    "facebook": {
+  "payload": {   
+    "kik": {
       "text": "Hello, Facebook!"
-    },
-    "slack": {
-      "text": "This is a text response for Slack."
-    }
-  },
-  "outputContexts": [
-    {
-      "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-      "lifespanCount": 5,
-      "parameters": {
-        "param": "param value"
-      }
-    }
-  ],
-  "followupEventInput": {
-    "name": "event name",
-    "languageCode": "en-US",
-    "parameters": {
-      "param": "param value"
     }
   }
 }
