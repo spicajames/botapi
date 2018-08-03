bc120bb9387b897463e8796674a521824e636f54
Spica <spicajames@users.noreply.github.com>
2018-08-03 09:57:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,24 +2,31 @@
 //$redis = new Predis\Client(getenv('REDIS_URL'));
 
 $my_file = 'file.txt';
-$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
-$data['prueba'] = 'ajaja';
-//$data = 'This is the data';
-fwrite($handle, json_encode($data));
-
-$my_file = 'file.txt';
 $handle = fopen($my_file, 'r');
 $dataIn = fread($handle,filesize($my_file));
 
-var_dump(json_decode($dataIn));
+//var_dump(json_decode($dataIn));
 
 header('Content-type: application/json');
 $data = json_decode(file_get_contents("php://input"));
 $intent = $data->queryResult->intent->displayName;
 if($intent == "get"){
   $key = $data->queryResult->parameters->any;
-  echo "{'fulfillmentText': 'You asked for $key'}";
+  $val = $dataIn[$key];
+  echo "{'fulfillmentText': '$val'}";
 } else if($intent == "post"){
+  $key = $data->queryResult->parameters->key;
+  $val = $data->queryResult->parameters->any;
+  
+  $data[$key] = $val;
+
+  $data = array_merge ($dataIn,$data);
+  
+ $my_file = 'file.txt';
+ $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+
+ fwrite($handle, json_encode($data));
+  
   echo "{'fulfillmentText': 'Got it!'}";
 } else{
   echo "{'fulfillmentText': 'I have no idea what your asking for'}";
