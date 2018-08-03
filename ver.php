0884740800dd8da1486a2d9c3c452cb28868116d
Spica <spicajames@users.noreply.github.com>
2018-08-03 11:19:11 -0500

    Update ver.php

--- ver.php
+++ ver.php
@@ -2,20 +2,7 @@
 
 $conn = pg_connect(getenv("DATABASE_URL"));
 $result = pg_query($conn, "CREATE TABLE memory (ID SERIAL PRIMARY KEY     NOT NULL, WORD  TEXT    NOT NULL,  MEANING  TEXT    NOT NULL);");
+var_dump($result);
 $result = pg_insert($conn, 'memory', array('WORD' => 'RULES', 'MEANING' => 'THIS'));
+var_dump($result);
 
-var_dump(($result));
-
-$my_file = 'file.txt';
-$handle = fopen($my_file, 'r');
-$dataIn = fread($handle,filesize($my_file));
-var_dump(($dataIn));
-
-$dataIn = array();
-$key = "bike";
-$val= "awsom";
-$data[$key] = $val;
-var_dump($data);
-$data = array_merge ($dataIn,$data);
-
-var_dump($data);
