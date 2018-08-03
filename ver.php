7e8f977ebf025538b80210a530ba230e8be9fedd
Spica <spicajames@users.noreply.github.com>
2018-08-03 11:38:20 -0500

    Update ver.php

--- ver.php
+++ ver.php
@@ -4,8 +4,21 @@ $conn = pg_connect(getenv("DATABASE_URL"));
 //$result = pg_query($conn, "CREATE TABLE memory (ID SERIAL PRIMARY KEY     NOT NULL, WORD  TEXT    NOT NULL,  MEANING  TEXT    NOT NULL);");
 //var_dump($result);
 //$result = pg_insert($conn, 'memory', array("WORD" => "RULES", "MEANING" => "THIS"));
- $insert = "INSERT INTO memory (WORD, MEANING) VALUES ('asddsa', 'male')";
-$result = pg_query($conn,$insert);          
-var_dump($result);
-var_dump(pg_last_error());
+ //$insert = "INSERT INTO memory (WORD, MEANING) VALUES ('asddsa', 'male')";
+//$result = pg_query($conn,$insert);          
+//var_dump($result);
+//var_dump(pg_last_error());
 
+
+$sql = "SELECT * FROM Countries";
+$result = pg_query($dbh, $sql);
+if (!$result) {
+   die("Error in SQL query: " . pg_last_error());
+}
+
+while ($row = pg_fetch_array($result)) {
+    var_dump($row);
+}
+pg_free_result($result);
+
+pg_close($dbh);
