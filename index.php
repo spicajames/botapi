c78b4f5766363c83546d349d0a3886ba311d0a9a
Spica <spicajames@users.noreply.github.com>
2018-08-03 14:36:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,27 +6,30 @@ if($intent == "get"){
   $key = $data->queryResult->parameters->any;
   $val = $dataIn[$key];
   echo "{'fulfillmentText': '$val'}";
-} else if($intent == "post"){
+} else if($intent == "post"){  
   $key = $data->queryResult->parameters->key;
   $val = $data->queryResult->parameters->any;
- 
-  $conn = pg_connect(getenv("DATABASE_URL"));
-  $select = "SELECT * FROM memory WHERE WORD = '$key'";
-  $resultS = pg_query($conn,$select);
-  $found = false;
-  while ($row = pg_fetch_array($resultS)) {
-    $found = $row['meaning'];
-  }
-  if($found == false){
-    $sql = "INSERT INTO memory (WORD, MEANING) VALUES ('$key', '$val')";
-  }else{
-     $sql = "UPDATE memory SET MEANING = '$val' WHERE WORD = '$key'";    
-  }
   
- $result = pg_query($conn,$sql); 
- pg_free_result($result);
- pg_close($conn);
-  echo "{'fulfillmentText': 'Got it! ".pg_last_error()."'}";
+  if($key != ""){
+    $conn = pg_connect(getenv("DATABASE_URL"));
+    $select = "SELECT * FROM memory WHERE WORD = '$key'";
+    $resultS = pg_query($conn,$select);
+    $found = false;
+    while ($row = pg_fetch_array($resultS)) {
+      $found = $row['meaning'];
+    }
+    if($found == false){
+      $sql = "INSERT INTO memory (WORD, MEANING) VALUES ('$key', '$val')";
+    }else{
+       $sql = "UPDATE memory SET MEANING = '$val' WHERE WORD = '$key'";    
+    }
+
+   $result = pg_query($conn,$sql); 
+   pg_free_result($result);
+   pg_close($conn);
+    echo "{'fulfillmentText': 'Got it!'}";
+  }
+  echo "{'fulfillmentText': 'Something went wrong, I didn't got that!'}";
 } else{
   echo "{'fulfillmentText': 'I have no idea what your asking for'}";
 }
