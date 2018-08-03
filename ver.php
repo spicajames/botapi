a7f2b9bd8d0d5c56693d0c5879d33feafc3f3c8e
Spica <spicajames@users.noreply.github.com>
2018-08-03 11:13:12 -0500

    Update ver.php

--- ver.php
+++ ver.php
@@ -1,7 +1,7 @@
 <?php 
 
 $conn = pg_connect(getenv("DATABASE_URL"));
-$result = pg_query($conn, "CREATE TABLE memory (ID SERIAL INT PRIMARY KEY     NOT NULL, WORD  TEXT    NOT NULL,  MEANING  TEXT    NOT NULL);");
+$result = pg_query($conn, "CREATE TABLE memory (ID SERIAL PRIMARY KEY     NOT NULL, WORD  TEXT    NOT NULL,  MEANING  TEXT    NOT NULL);");
 $result = pg_insert($conn, 'memory', array('WORD' => 'RULES', 'MEANING' => 'THIS'));
 
 var_dump(($result));
