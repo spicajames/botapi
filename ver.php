d20a796c6fb005ae860c510ec6f473193aaca037
Spica <spicajames@users.noreply.github.com>
2018-08-03 10:50:57 -0500

    Update ver.php

--- ver.php
+++ ver.php
@@ -2,4 +2,13 @@
 $my_file = 'file.txt';
 $handle = fopen($my_file, 'r');
 $dataIn = fread($handle,filesize($my_file));
-var_dump(json_decode($dataIn));
+var_dump(($dataIn));
+
+$dataIn = array();
+$key = "bike";
+$val= "awsom";
+$data[$key] = $val;
+var_dump($data);
+$data = array_merge ($dataIn,$data);
+
+var_dump($data);
